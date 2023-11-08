045ab48f164f5bcf6814a2fbe9348c54bce79ee0
julyxr <adipha8998@gmail.com>
2023-11-08 10:48:42 +0700

    from.html

---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+    $name = "Adipha";
+    $department = "Business Computer";
+
+    echo $name ." ". $department;
+
+    $num1 = "100";
+    $num2 = "200";
+    $num3 = "300";
+    $num4 = "400";
+
+    $num = $num1 + $num2 + $num3 + $num4;
+    echo "sum = ".$sum."<br>";
+    $avg = $sum / 4;
+    echo $avg = ".$avg";
+    
+    #x++ คือการที่ x+1 จากนั้นเก็บค่าใน x เหมือนเดิม
+    $x = 5;
+    echo"x= ".$x."<br>";
+    $x++;
+    echo"x= ".$x."<br>";
