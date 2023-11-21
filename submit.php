70edee7fa1041a9bf68cffed53259324267167cd
julyxr <adipha8998@gmail.com>
2023-11-21 11:10:39 +0700

    add

--- submit.php
+++ submit.php
@@ -1,23 +1,30 @@
 <?php
-    $email = $_POST['$email'];
-    $password = $_POST['$password'];
-    $city = $_POST['$city'];
-    $web = $_POST['$web'];
-    $role = $_POST['$role'];
-    $sign = $_POST['$sign'];
-    $sign1 = $_POST['$sign1'];
-    $sign2 = $_POST['$sign2'];
-    $sign3 = $_POST['3'];
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+   $city = $_POST['city'];
+   $web = $_POST['web'];
+   $role = $_POST['role'];
+   
 
-    echo 'email: '.$email.'<br>';
-    echo'password: '.$password.'<br>';
-    echo 'city:' .$city.'<br>';
-    echo 'web:' .$web.'<br>';
-    echo 'role:' .$role.'<br>';
-    echo 'sign:' .$sign.'<br>';
-    echo 'sign1'.$sign1.'<br>';
-    echo 'sign2'.$sign2.'<br>';
-    echo 'sign3'.$sign3.'<br>';
+   $servername = 'localhost:80, 443';
+   $username = 'root';
+   $password = '123456';
+   $dbname = 'db_employee';
 
+   $conn = new mysqli($servername, $username, $password);
+   if ($conn->connect_error) {
+    die(''. $conn->connect_error);
+   }
 
-?>a
+   $sql = 'INSERT INTO employee (username, password, city, webserver, role)
+           VALUES ($email,$password,$city,$web,$role)';
+        
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+    } else {
+        echo'error'.$sql.'<br>'.$conn->error;
+    }
+    $conn->close();
+?>
