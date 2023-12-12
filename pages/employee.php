5a704f7db81fdbc043cfe8c32dfe5b7e137f08cf
julyxr <adipha8998@gmail.com>
2023-12-12 09:45:20 +0700

    add

---
+++ pages/employee.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ข้อมูลพนักงาน</h1>
+    <table>
+        <tr>
+            <th>Username</th>
+            <th>City</th>
+            <th>Webserver</th>
+            <th>role</th>
+        </tr>
+        <tr>
+        <tr>
+            <th>123</th>
+            <th>456</th>
+            <th>google</th>
+            <th>admin</th>
+        </tr>
+    </table>
+</body>
+</html>
